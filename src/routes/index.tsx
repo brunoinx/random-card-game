1165749ad030ce3186019a126121e1a4d66f1102
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-03-07 20:44:54 -0300

    feat: :sparkles: initialize game page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../screens/Home';
+import { GameCards } from '../screens/GameCards';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/game" element={<GameCards />} />
     </Routes>
   );
 }
---
+++ src/screens/GameCards/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+export function GameCards() {
+  return (
+    <div>
+      <h3>Jogo de Carta</h3>
+    </div>
+  );
+}
