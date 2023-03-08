f181ae779008888c170e27d0b48ed93b92895cbd
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-03-08 18:14:14 -0300

    feat: ✨ finishing game cards page

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,17 @@
 import axios from 'axios'
 
+export interface ResponseProps {
+  cards: CardResponseProps[];
+  deck_id: string;
+}
+
+export interface CardResponseProps {
+  code: string;
+  image: string;
+  suit: 'DIAMONDS' | 'CLUBS' | 'HEARTS' | 'SPADES';
+  value: number;
+}
+
 const spadesCards = "AS,2S,3S,4S,5S,6S,7S,8S,9S,0S,JS";
 const diamondsCards = "AD,2D,3D,4D,5D,6D,7D,8D,9D,0D,JD";
 const clubsCards = "AC,2C,3C,4C,5C,6C,7C,8C,9C,0C,JC";
--- src/screens/GameCards/index.tsx
+++ src/screens/GameCards/index.tsx
@@ -1,9 +1,129 @@
+import { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 
+import { Button } from '../../components/Button';
+import { Card, CardProps } from '../../components/Card';
+
+import { inputNameKey } from '../../storage/keys';
+import { nameCardSwitch } from '../../utils/nameCardSwitch';
+import {
+  api,
+  COUNT_PARAM,
+  PARTIAL_DECK_PARAM,
+  ResponseProps,
+  CardResponseProps,
+} from '../../lib/axios';
+
 export function GameCards() {
+  const [playerName, setPlayerName] = useState('');
+  const [cards, setCards] = useState<CardProps[]>([]);
+  const [deckId, setDeckId] = useState('');
+  const [countCardsAdded, setCountCardsAdded] = useState(3);
+  const [isShuffled, setIsShuffled] = useState(false);
+
+  useEffect(() => {
+    const namePlayer = window.localStorage.getItem(inputNameKey);
+    if (namePlayer) setPlayerName(namePlayer);
+  }, []);
+
+  useEffect(() => {
+    fetchNewDeck();
+  }, []);
+
+  async function fetchNewDeck() {
+    try {
+      if (cards.length > 0) return;
+
+      const { data } = await api.get<ResponseProps>('new/shuffle', {
+        params: {
+          cards: PARTIAL_DECK_PARAM,
+        },
+      });
+
+      setDeckId(data.deck_id);
+
+      const partialDeck = await api.get<ResponseProps>(`${data.deck_id}/draw`, {
+        params: {
+          count: COUNT_PARAM,
+        },
+      });
+
+      const cardData = getFormatCardData(partialDeck.data.cards);
+
+      setCards(cardData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function getFormatCardData(cardsResponse: CardResponseProps[]) {
+    return cardsResponse.map(card => {
+      const nameCard = nameCardSwitch(card.code);
+
+      return {
+        code: card.code,
+        name: nameCard,
+        description: 'descrição da carta',
+        image: card.image,
+        value: card.value,
+      };
+    });
+  }
+
+  async function handleAddNewCard() {
+    try {
+      if (cards.length === 8) return;
+
+      const buyCard = await api.get<ResponseProps>(`${deckId}/draw`, {
+        params: {
+          count: 1,
+        },
+      });
+
+      const formattedCardData = getFormatCardData(buyCard.data.cards);
+
+      setCards(prevState => [...prevState, ...formattedCardData]);
+      setCountCardsAdded(prev => prev - 1);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleShuffleCards() {
+    const shuffledCards = cards.sort(() => Math.random() - 0.5);
+    setCards(shuffledCards);
+
+    setIsShuffled(prev => !prev);
+  }
+
   return (
-    <div>
-      <h3>Jogo de Carta</h3>
+    <div className={styles.container}>
+      <h2 className={styles.player}>Nome do Jogador: {playerName}</h2>
+
+      <div className={styles.cards}>
+        {cards.map(card => (
+          <Card
+            key={card.code}
+            name={card.name}
+            description={card.description}
+            image={card.image}
+            value={card.value}
+          />
+        ))}
+      </div>
+
+      <div className={styles.buttons}>
+        <div className={styles.btnAdd}>
+          <Button
+            name="Puxar nova carta"
+            onClick={handleAddNewCard}
+            disabled={cards.length === 8}
+          />
+
+          <p>Compras restantes: {countCardsAdded}</p>
+        </div>
+        <Button name="Embaralhar" onClick={handleShuffleCards} />
+      </div>
     </div>
   );
 }
