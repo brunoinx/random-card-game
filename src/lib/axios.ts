a6be537b2fa8b788c855b00fdb9c4e172899c169
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-03-08 12:39:07 -0300

    refactor: :recycle: change request base url api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,9 +5,9 @@ const diamondsCards = "AD,2D,3D,4D,5D,6D,7D,8D,9D,0D,JD";
 const clubsCards = "AC,2C,3C,4C,5C,6C,7C,8C,9C,0C,JC";
 const heartsCards = "AH,2H,3H,4H,5H,6H,7H,8H,9H,0H,JH";
 
+export const PARTIAL_DECK_PARAM = `${spadesCards},${diamondsCards},${clubsCards},${heartsCards}`;
+export const COUNT_PARAM = 5;
+
 export const api = axios.create({
   baseURL: 'https://deckofcardsapi.com/api/deck/',
-  params: {
-    cards: `${spadesCards},${diamondsCards},${clubsCards},${heartsCards}`
-  }
 })
